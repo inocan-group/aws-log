d72f065e923fed1ce360d9ccc590f4c26bec01ab
Robinson Villegas <kento@Robinsons-MacBook-Pro-2.local>
2020-10-02 17:28:05 +0200

    refactor invoke

--- lib/es/invoke/ensureFunctionName.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function ensureFunctionName(name: string): string;
--- lib/es/stepFunction.d.ts
+++
@@ -1,2 +0,0 @@
-import { IDictionary } from "common-types";
-export declare function stepFunction(stepArn: string, request: IDictionary, options?: IDictionary): Promise<unknown>;
