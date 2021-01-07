4ead1785bded2b17a77ef860db4e1a3ded22da09
Ken Snyder <ken@ken.net>
2021-01-07 15:13:09 -0800

    refactor: remove "lib" from repo

--- lib/cjs/logger/lambda.d.ts
+++
@@ -1,4 +0,0 @@
-import { IDictionary, IAWSLambdaProxyIntegrationRequest, IAWSLambaContext } from "common-types";
-import { ILoggerApi } from "./logging-api";
-export declare type ILambdaEvent<T = IDictionary> = IAWSLambdaProxyIntegrationRequest | T;
-export declare function lambda(event: ILambdaEvent, ctx: IAWSLambaContext, options?: IDictionary): ILoggerApi;
--- lib/es/shared/sample.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * **sample**
- *
- * samples a random number and sees if it is within the
- * sampling rate, if so it returns `all` otherwise `none`
- */
-export declare function sample(samplingRate?: number): "all" | "none";
